b5f78780ebbaff783f271bcbf8015450f57b34b2
DevOpsPolska <devops.polska@gmail.com>
2017-06-11 19:04:47 +0200

    change in upgrade

--- demo_install_dependencies.sh
+++ demo_install_dependencies.sh
@@ -4,5 +4,5 @@ echo "Start" > $HOME/customscript.log
 echo "update" >> $HOME/customscript.log
 apt-get update
 echo "upgrade" >> $HOME/customscript.log
-apt-get upgrade -y
+apt-get -y upgrade
 echo "Finish" >> $HOME/customscript.log
