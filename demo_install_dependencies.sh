6876af254e1f6e79bbec5b3d761de1fad7d6d36d
DevOpsPolska <devops.polska@gmail.com>
2017-06-12 01:36:34 +0200

    add sudo and id

--- demo_install_dependencies.sh
+++ demo_install_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #initial system update
 echo "Start" > $HOME/customscript.log
+id -a >> $HOME/customscript.log
 echo "update" >> $HOME/customscript.log
 sudo apt-get update
 echo "upgrade" >> $HOME/customscript.log
-#apt-get -y upgrade
+sudo apt-get -y upgrade
 echo "Finish" >> $HOME/customscript.log
