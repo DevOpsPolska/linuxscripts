1fb8dcce17666fc11105a78207a3ba43e590eddd
DevOpsPolska <DevOpsPolska@users.noreply.github.com>
2017-06-12 15:23:45 +0200

    shell check

--- demo_install_dependencies.sh
+++ demo_install_dependencies.sh
@@ -5,6 +5,7 @@ id -a >> $HOME/customscript.log
 echo "update" >> $HOME/customscript.log
 sudo apt-get update
 sudo apt-get install tree
+echo $0 >> $HOME/customscript.log
 echo "upgrade" >> $HOME/customscript.log
 sudo apt-get -y upgrade
 echo "Finish" >> $HOME/customscript.log
