ba99396e1e452e13778d0fe03707bcd2bc037cbc
DevOpsPolska <DevOpsPolska@users.noreply.github.com>
2017-06-12 18:18:48 +0200

    add java environment installation

--- demo_install_dependencies.sh
+++ demo_install_dependencies.sh
@@ -2,10 +2,23 @@
 #initial system update
 echo "Start" > $HOME/customscript.log
 id -a >> $HOME/customscript.log
+echo $0 >> $HOME/customscript.log
 echo "update" >> $HOME/customscript.log
 sudo apt-get update
+
+echo "installation" >> $HOME/customscript.log
 sudo apt-get install tree
-echo $0 >> $HOME/customscript.log
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jre -y
+sudo apt-get install mongodb -y
+sudo apt-get install tomcat7 -y
+sudo apt-get install wget -y
+
+# Set Java environment variables
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
+
 echo "upgrade" >> $HOME/customscript.log
-sudo apt-get -y upgrade
+# issue with custom script or waagent do not upgarde
+# sudo apt-get -y upgrade
 echo "Finish" >> $HOME/customscript.log
